c1988014aa881ff25c430858596da3a3fed4bb85
Yuri Gabriel Constantino <yuri.gconstantino@gmail.com>
2024-10-07 12:22:11 -0300

    Alterações feitas

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,11 @@
-import styles from './Hero.module.css'
+import { Form, H2Title } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+  <Form>
+    <H2Title>
+      As melhores vagas para tecnologia, design e artes visuais.
+    </H2Title>
+  </Form>
 )
 
 export default Hero
